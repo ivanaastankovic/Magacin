d8331bda0feaf7626e62f3bed90b1e88566ed945
ivanaastankovic <stankovic.ivana1998@gmail.com>
2021-03-13 22:14:16 +0100

    Update-ovana klasa Magacin - implementira interfejs MagacinInterface.

--- src/magacin/Magacin.java
+++ src/magacin/Magacin.java
@@ -2,6 +2,26 @@ package magacin;
 
 import java.util.LinkedList;
 
-public class Magacin {
+import magacininterfejs.MagacinInterface;
+
+public class Magacin implements MagacinInterface{
 	public LinkedList<Artikal> artikli= new LinkedList<Artikal>();
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal(int kolicina) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
